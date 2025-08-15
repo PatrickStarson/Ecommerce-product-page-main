1c956657b0e6d89b2af4823c6c9157c7d962db3a
PatrickStarson <spartak.mamiev@gmail.com>
2025-08-15 13:46:57 +0200

    Add event listener to the cart icon to display or hidde the cart container.

--- script.js
+++ script.js
@@ -38,3 +38,24 @@ thumbnails.forEach((img, index) => {
 });
 
 updateSlide();
+
+//Add event listener to the cart icon to display or hidde the cart container
+
+const cartContainer = document.querySelector('.cart-container');
+const cartIconBtn = document.querySelector('#cart-icon-btn');
+
+cartIconBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  cartContainer.style.display = cartContainer.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', (e) => {
+  if(!cartContainer.contains(e.target) && e.target !== cartIconBtn) {
+    cartContainer.style.display = 'none';
+  }
+})
+
+
+
+//Create a itemsAmount viable to store amount of items in the cart
+//Add event listeners to the - and + buttons, and make them change the number of items displayed and store the amount in the itemsAmount
