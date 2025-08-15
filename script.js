96f28b376a170ec92fdf6de6a6531c1aa9dcba6c
PatrickStarson <spartak.mamiev@gmail.com>
2025-08-15 12:27:37 +0200

    Change the logic to be able to switch between images and also highlight the thumbnail of the image active.

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const previousBtn = document.querySelector('#previous-btn')
-const nextBtn = document.querySelector('#next-btn')
+const previousBtn = document.querySelector('#previous-btn');
+const nextBtn = document.querySelector('#next-btn');
 const slides = [
   "images/image-product-1.jpg",
   "images/image-product-2.jpg",
   "images/image-product-3.jpg",
   "images/image-product-4.jpg"
-]
+];
+
+const imageElement = document.querySelector('#product-image-large');
+
+const thumbnails = document.querySelectorAll('.product-image-thumbnail img');
 
 let currentIndex = 0;
-const imageElement = document.querySelector('#product-image-large');
+
+function updateSlide() {
+  imageElement.src = slides[currentIndex];
+  thumbnails.forEach(img => img.classList.remove('active-thumbnail'));
+  thumbnails[currentIndex].classList.add('active-thumbnail');
+}
 
 previousBtn.addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  imageElement.src = slides[currentIndex];
+  updateSlide();
 });
 
 nextBtn.addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % slides.length;
-  imageElement.src = slides[currentIndex];
+  updateSlide();
 });
+
+thumbnails.forEach((img, index) => {
+  img.parentElement.addEventListener('click', e => {
+    e.preventDefault(); 
+    currentIndex = index;
+    updateSlide();
+  });
+});
+
+updateSlide();
